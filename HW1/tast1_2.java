9f230ef15874ccf1de805dfeb4fa1544cd651110
Ekaterina <missathlon08@gmail.com>
2023-04-17 17:40:58 +0300

    Task 2

---
+++ HW1/tast1_2.java
@@ -0,0 +1,21 @@
+// Вывести все простые числа от 1 до 1000
+
+public class tast1_2 {
+    public static void main(String[] args) {
+        int i, N, t, k;
+        N = 1000;
+        for (k = 2; k <= N; k++) {
+            i = 2;
+            t = 0;
+            while (i < k) {
+                if (k % i == 0) {
+                    t++;
+                }
+                i++;
+            }
+            if (t == 0) {
+                System.out.println(k);
+            }
+        }
+    }
+}
